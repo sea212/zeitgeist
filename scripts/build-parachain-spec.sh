c58c214597cbfc92006f6a943c5cc1dc151ae40f
Harald Heckmann <mail@haraldheckmann.de>
2022-11-10 13:03:40 +0100

    Add zeitgeist (kusama slot swap) genesis files

--- scripts/build-parachain-spec.sh
+++ scripts/build-parachain-spec.sh
@@ -5,23 +5,23 @@
 set -euxo pipefail
 
 # For example, node/res/bs_parachain.json
-OUTPUT_FILE=
+OUTPUT_FILE="node/res/zeitgeist_parachain.json"
 # For example, "Battery Station"
-PROD_CHAIN_NAME=
+PROD_CHAIN_NAME="Zeitgeist (slot swap)"
 # For example, battery_station
-PROD_CHAIN_PROTOCOL_ID=
+PROD_CHAIN_PROTOCOL_ID="zeitgeist_kusama_slot_swap_v1"
 # For example, battery_station_mainnet
-PROD_CHAIN_ID=
+PROD_CHAIN_ID="zeitgeist_kusama_slot_swap_v1"
 # For example, battery_station_staging
-STAGE_CHAIN=
+STAGE_CHAIN="zeitgeist_staging"
 
-cargo build --profile=production --bin zeitgeist --features parachain
-./target/production/zeitgeist build-spec --chain $STAGE_CHAIN --disable-default-bootnode > $OUTPUT_FILE
+cargo build --profile=release --bin zeitgeist --features parachain
+./target/release/zeitgeist build-spec --chain $STAGE_CHAIN --disable-default-bootnode > $OUTPUT_FILE
 
 sed -i "s/\"id\": \".*\"/\"id\": \"$PROD_CHAIN_ID\"/" $OUTPUT_FILE
 sed -i "s/\"name\": \".*\"/\"name\": \"$PROD_CHAIN_NAME\"/" $OUTPUT_FILE
 sed -i "s/\"protocolId\": \".*\"/\"protocolId\": \"$PROD_CHAIN_PROTOCOL_ID\"/" $OUTPUT_FILE
 
-./target/production/zeitgeist build-spec --chain $OUTPUT_FILE --disable-default-bootnode --raw > $OUTPUT_FILE.raw
+./target/release/zeitgeist build-spec --chain $OUTPUT_FILE --disable-default-bootnode --raw > $OUTPUT_FILE.raw
 rm -f $OUTPUT_FILE
 mv $OUTPUT_FILE.raw $OUTPUT_FILE
